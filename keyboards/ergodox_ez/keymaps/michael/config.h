c07de693b0564bd6bc8c5748f572ba0fa998f534
Michael McClimon <michael@mcclimon.org>
2019-07-22 22:16:28 -0400

    Tweak config.h and add rules.mk file

--- keyboards/ergodox_ez/keymaps/michael/config.h
+++ keyboards/ergodox_ez/keymaps/michael/config.h
@@ -2,6 +2,8 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#pragma once
+
 #undef TAPPING_TERM
 #define TAPPING_TERM 165
 
