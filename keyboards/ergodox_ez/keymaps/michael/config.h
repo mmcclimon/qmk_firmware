4bdb792630b4376cecac806ff04f51c483479b6e
Michael McClimon <michael@mcclimon.org>
2019-07-22 20:17:48 -0400

    Import config from ergodox ez configurator

---
+++ keyboards/ergodox_ez/keymaps/michael/config.h
@@ -0,0 +1,11 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 165
+
+#define PERMISSIVE_HOLD
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+
